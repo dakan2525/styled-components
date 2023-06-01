b0b0e3da4e45ddf22eafb207ee77b879144e0b62
Andres Prado <107523609+dakan2525@users.noreply.github.com>
2023-05-31 20:45:28 -0500

    cargue para web

---
+++ build/precache-manifest.789685a4f23b1543c4bf7a2e384056a6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5642455e5a70f734b2563106d2f6ea7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b173ed6d37017d99570e",
+    "url": "/static/css/main.3826277b.chunk.css"
+  },
+  {
+    "revision": "1ceee1de7e49da6280f1",
+    "url": "/static/js/2.d4282607.chunk.js"
+  },
+  {
+    "revision": "82e8035e49740f791fa94175bc04ac4e",
+    "url": "/static/js/2.d4282607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b173ed6d37017d99570e",
+    "url": "/static/js/main.e4090511.chunk.js"
+  },
+  {
+    "revision": "d08efe7b38db90f3425a",
+    "url": "/static/js/runtime-main.82ada07f.js"
+  },
+  {
+    "revision": "44dbaa78fb2ed240792c9e7a424b0ea0",
+    "url": "/static/media/alimentacion.44dbaa78.svg"
+  },
+  {
+    "revision": "b97c300bed11302a8687c16e07aa89e5",
+    "url": "/static/media/dinero.b97c300b.svg"
+  },
+  {
+    "revision": "f3a2bff49642ca68b3841fde422295cb",
+    "url": "/static/media/logo.f3a2bff4.svg"
+  },
+  {
+    "revision": "acd41ceb3366a71c61b7ed3d5df83682",
+    "url": "/static/media/ojo.acd41ceb.svg"
+  },
+  {
+    "revision": "17e95f3cc45dc23ffda7f4d29dc137df",
+    "url": "/static/media/otros.17e95f3c.svg"
+  },
+  {
+    "revision": "134d853a95b2b30676da2c63cda2eeaf",
+    "url": "/static/media/privado.134d853a.svg"
+  },
+  {
+    "revision": "605ef9ca7e70a1bb568ffcec443d05f6",
+    "url": "/static/media/salud.605ef9ca.svg"
+  },
+  {
+    "revision": "0981f5bfd5f197c52e678c3c0616cd81",
+    "url": "/static/media/themeOff.0981f5bf.svg"
+  },
+  {
+    "revision": "75ea28146734a5ea6593dab45d272a90",
+    "url": "/static/media/themeOn.75ea2814.svg"
+  },
+  {
+    "revision": "15a4ed7310630a72e21a8054897c3a51",
+    "url": "/static/media/transporte.15a4ed73.svg"
+  },
+  {
+    "revision": "59035811601fe0a607827eb6408e14ef",
+    "url": "/static/media/utilidades.59035811.svg"
+  }
+]);
